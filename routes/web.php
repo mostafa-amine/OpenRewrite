33c3c622b3c7c0d8b722bdc427aeaf4fa65c5f55
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-12-30 21:04:05 +0100

    :white_check_mark: update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RewriterController;
+use App\Http\Livewire\Rewriter\Rewrite;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,14 +20,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/profile' , [ProfileController::class , 'setToken'])->name('profile.setToken');
+
+    // Rewriter
+    Route::get('/rewriter' , [RewriterController::class , 'index'])->name('rewriter');
+    Route::get('/rewriter/result' , [RewriterController::class , 'result'])->name('rewriter.result');
 });
 
 require __DIR__.'/auth.php';
