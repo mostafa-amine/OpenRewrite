43ff26b988125cf1894887364e801216a3473cde
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2022-12-30 21:01:14 +0100

    :sparkles: create open ai class service

---
+++ app/Services/OpenaiService.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Services;
+
+use OpenAI;
+
+
+class OpenaiService
+{
+    public static function generate($apiToken , $userText)
+    {
+        $client = OpenAI::client($apiToken);
+
+        $result = $client->completions()->create([
+            "model" => "text-davinci-003",
+            "temperature" => 0.7,
+            "top_p" => 1,
+            "frequency_penalty" => 0,
+            "presence_penalty" => 0,
+            'max_tokens' => 600,
+            'prompt' => sprintf($userText),
+        ]);
+
+        $content = trim($result['choices'][0]['text']);
+
+        return $content;
+    }
+}
